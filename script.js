9a8feb261582f7bf9cebea8a9fc485e90e17df44
99dilmimaleesha <dilmimaleesha300@gmail.com>
2025-06-20 16:04:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const names = ["alice" , "bob", "charlie"];
+
+const namesupper = names.map(firtsletterUpper);
+
+console.log(namesupper);
+
+function firtsletterUpper(element){
+     return element.charAt(upperfirstLetter);
+}
