468219569c73347e098a3dff1aca169419e255a8
99dilmimaleesha <dilmimaleesha300@gmail.com>
2025-06-26 17:24:51 +0530

    Changed portfolio to react

--- script.js
+++
@@ -1,9 +0,0 @@
-const names = ["alice" , "bob", "charlie"];
-
-const namesupper = names.map(firtsletterUpper);
-
-console.log(namesupper);
-
-function firtsletterUpper(element){
-     return element.charAt(upperfirstLetter);
-}
